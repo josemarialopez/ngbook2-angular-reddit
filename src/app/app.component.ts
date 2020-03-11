1a44d5220eef4d191cc8789bbf1de9c4b2d4d4fd
José María López <josemaria.lopez.palomo@toptal.com>
2020-03-11 19:12:49 +0100

    Final version of the second extercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Article } from './article/article.model';
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  articles: Article[];
+
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) =>  b.votes - a.votes );
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    this.articles.push(new Article(title.value, link.value));
+    this.resetInputs([title, link]);
+    return false; 
+  }
+
+  resetInputs(inputs) {
+    for (let input of inputs) {
+      input.value = '';
+    }
+  }
 }
